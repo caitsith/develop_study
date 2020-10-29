50ece5106ef85fc8a2a9651505bc42bad3096a0b
yuinoue <room.caitsith@gmail.com>
2020-10-29 16:34:40 +0900

    vuex : apiリクエスト

--- vue_study/step01/my-project/src/store/index.js
+++ vue_study/step01/my-project/src/store/index.js
@@ -6,16 +6,29 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     globalCount1: 0,
-    globalCount2: 0
+    globalCount2: 0,
+    profile: null,
   },
   mutations: {
     globalIncrement(state){
       state.globalCount1++,
       state.globalCount2--
-    }
+    },
+    setGithubProfile(state, payload){
+      state.profile = payload;
+    },
   },
   actions: {
+    async fetchGithubProfile(store, payload){
+      if(store.state.profile !== null) return;
+
+      const response = await fetch(
+        `https://api.github.com/users/${payload.user_id}`
+      ).then((res) => {
+        return res.json();
+      });
+      store.commit("setGithubProfile",response);
+    },
   },
-  modules: {
-  }
+  modules: {},
 })
