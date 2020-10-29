13b8c612dfae28bf429a00f42404991275557071
yuinoue <room.caitsith@gmail.com>
2020-10-29 10:58:16 +0900

    ver2.0
    - SoftwareDesign 2020-09 : STEP02完了

--- vue_study/step01/my-project/src/App.vue
+++ vue_study/step01/my-project/src/App.vue
@@ -1,25 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <p>Hello my vue</p>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Counter name="Counter 1" :initCount="5" />
-    <Counter name="Counter 2" :initCount="10" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/count">Count</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import Counter from "./components/Counter.vue";
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Counter
-  }
-}
-</script>
 
 <style>
 #app {
@@ -28,6 +16,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
